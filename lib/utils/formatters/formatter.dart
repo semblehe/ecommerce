87cc93e7a58e3a94096d37cf040f3fb71ecff844
dedyunej <dedy3@unej.ac.id>
2024-04-26 16:30:27 +0700

    formater dan helper

---
+++ lib/utils/formatters/formatter.dart
@@ -0,0 +1,16 @@
+import 'package:intl/intl.dart';
+
+class TFormatter {
+
+  static String formatDate(DateTime date) {
+    date ??= DateTime.now();
+    return DateFormat('dd/MM/yyyy').format(date);
+  }
+  static String formatCurrency(double amount) {
+    return NumberFormat.currency(locale: 'en_US', symbol: '₦').format(amount);
+  }
+
+
+
+
+}
